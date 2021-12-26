8ff0c15c703d117638254e3b0e747e4a8a755c2a
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2021-12-26 18:01:54 +0530

    added insertion sort color switcher

--- projects/sorting/src/app/insertion-sort/insertion-sort.component.ts
+++ projects/sorting/src/app/insertion-sort/insertion-sort.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BarGraphModel } from '../shared';
+import { activeElementColor, BarGraphModel, resetElementColor, timer } from '../shared';
 
 @Component({
     selector: 'app-insertion-sort',
@@ -12,7 +12,7 @@ export class InsertionSortComponent implements OnInit {
     maxNumber = 3000;
     minNumber = 100;
     Values: Array<BarGraphModel> = [];
-    delay = 500;
+    delay = 100;
     btnDisabled = false;
 
     constructor() { }
@@ -23,7 +23,7 @@ export class InsertionSortComponent implements OnInit {
 
     async initializeValue() {
         this.Values = [];
-        await this.timer(100);
+        await timer(100);
         const A = 'A'.charCodeAt(0);
         for (let i = 0; i < 26; i++) {
             this.Values.push({ value: this.getRandomNumber(), color: this.getRandomColor(), size: '', legend: String.fromCharCode(A + i) });
@@ -44,18 +44,22 @@ export class InsertionSortComponent implements OnInit {
             const key = arrayToSort[i];
             let j = i - 1;
             while (j >= 0 && arrayToSort[j].value > key.value) {
+                let element1Color = arrayToSort[j].color;
+                activeElementColor(arrayToSort, j);
+                await timer(this.delay);
                 arrayToSort[j + 1] = arrayToSort[j];
+                await timer(this.delay);
+                resetElementColor(arrayToSort, j + 1, element1Color);
                 j -= 1;
             }
+            
             if (arrayToSort[j + 1].value != key.value) {
+                resetElementColor(arrayToSort, i, key.color);
                 arrayToSort[j + 1] = key;
-                await this.timer(this.delay);
+                await timer(this.delay);
             }
         }
+        await timer(100);
         window.alert('Insertion sort completed');
-    }
-
-    timer(milliSeconds: number) {
-        return new Promise(res => setTimeout(res, milliSeconds));
     }
 }
