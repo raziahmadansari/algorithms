891b8e897bcc83c4a20204fcd9f176140af4b8da
Razi Ahmad Ansari <raziahmad.abd@gmail.com>
2021-12-26 19:49:19 +0530

    added color switcher in quickSort

--- projects/sorting/src/app/quick-sort/quick-sort.component.ts
+++ projects/sorting/src/app/quick-sort/quick-sort.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BarGraphModel } from '../shared';
+import { activeElementColor, BarGraphModel, resetElementColor, swap, timer } from '../shared';
 
 @Component({
     selector: 'app-quick-sort',
@@ -12,7 +12,7 @@ export class QuickSortComponent implements OnInit {
     maxNumber = 3000;
     minNumber = 100;
     Values: Array<BarGraphModel> = [];
-    delay = 500;
+    delay = 150;
     btnDisabled = false;
     pendingRecursiveCall = 0;
 
@@ -24,7 +24,7 @@ export class QuickSortComponent implements OnInit {
 
     async initializeValue() {
         this.Values = [];
-        await this.timer(100);
+        await timer(100);
         const A = 'A'.charCodeAt(0);
         for (let i = 0; i < 26; i++) {
             this.Values.push({ value: this.getRandomNumber(), color: this.getRandomColor(), size: '', legend: String.fromCharCode(A + i) });
@@ -43,6 +43,7 @@ export class QuickSortComponent implements OnInit {
     async beginQuickSort(arrayToSort: Array<BarGraphModel>) {
         this.pendingRecursiveCall = 0;
         await this.quickSort(arrayToSort, 0, arrayToSort.length - 1);
+        await timer(100);
         window.alert('Quick sort finished');
     }
 
@@ -56,27 +57,35 @@ export class QuickSortComponent implements OnInit {
 
     async partition(arrayToSort: Array<BarGraphModel>, low: number, high: number) {
         const pivot = arrayToSort[high];
+        const pivotColor = arrayToSort[high].color;
         pivot.legend += '*P';
+        activeElementColor(arrayToSort, high);
+        await timer(this.delay);
         let i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
         for (let j = low; j <= high - 1; j++) {
+            let element1Color = arrayToSort[j].color;
+            let element2Color = arrayToSort[i + 1].color;
+            activeElementColor(arrayToSort, j);
+            activeElementColor(arrayToSort, i + 1);
+            await timer(this.delay);
             if (arrayToSort[j].value < pivot.value) { // If current element is smaller than the pivot
                 i += 1; // increment index of smaller element
-                if (i != j) await this.swap(arrayToSort, i, j);
+                swap(arrayToSort, i, j);
+                resetElementColor(arrayToSort, i, element1Color);
+                resetElementColor(arrayToSort, j, element2Color);
+                await timer(this.delay);
+            } else {
+                resetElementColor(arrayToSort, j, element1Color);
+                resetElementColor(arrayToSort, i + 1, element2Color);
             }
         }
-        if ((i + 1) != high) await this.swap(arrayToSort, i + 1, high);
+        if ((i + 1) != high) {
+            swap(arrayToSort, i + 1, high);
+            await timer(this.delay);
+        }
         pivot.legend = pivot.legend.substring(0, 1);
+        pivot.color = pivotColor;
+        await timer(this.delay);
         return (i + 1);
-    }
-
-    async swap(array: Array<BarGraphModel>, index1: number, index2: number) {
-        const temp = array[index1];
-        array[index1] = array[index2];
-        array[index2] = temp;
-        await this.timer(this.delay);
-    }
-
-    timer(milliSeconds: number) {
-        return new Promise(res => setTimeout(res, milliSeconds));
     }
 }
